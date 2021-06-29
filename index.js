1c5448be2f85a475a1cfe93a765621ee758819f5
jonkarrer <karrerjon@gmail.com>
2021-06-29 14:13:24 -0500

    change header color on scroll

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import { dropMenu } from "./utils/navDrop.js";
 
 document.getElementById("burger").addEventListener("click", () => dropMenu());
+
+//Change header color
+window.onscroll = () => {
+  const header = document.getElementById("header");
+  header.style.background = `rgba(231, 222, 217, ${
+    window.scrollY / document.documentElement.clientHeight
+  })`;
+};
