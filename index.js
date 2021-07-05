1239defc5f77fd35c756b2462ae454c574a3ff60
jonkarrer <karrerjon@gmail.com>
2021-07-05 12:35:40 -0500

    footer done

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ document.getElementById("burger").addEventListener("click", () => dropMenu());
 //Change header color
 window.onscroll = () => {
   const header = document.getElementById("header");
+  let target = document.documentElement.clientHeight * 4;
+
+  if (window.scrollY > target) {
+    header.style.background = "rgb(105, 88, 83)";
+    return;
+  }
   header.style.background = `rgba(152, 135, 128, ${
     window.scrollY / document.documentElement.clientHeight
   })`;
